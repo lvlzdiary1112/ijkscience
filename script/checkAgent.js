56abd73d0983b560e4347aa8ee363c188587537c
lvlzdiary1112 <70059794+lvlzdiary1112@users.noreply.github.com>
2020-11-21 15:30:53 +0900

    Update checkAgent.js

--- script/checkAgent.js
+++ script/checkAgent.js
@@ -1,7 +1,7 @@
 var mobileKeyWords = new Array('iPhone', 'iPod', 'BlackBerry', 'Android', 'Windows CE', 'Windows CE;', 'LG', 'MOT', 'SAMSUNG', 'SonyEricsson', 'Mobile', 'Symbian', 'Opera Mobi', 'Opera Mini', 'IEmobile');
 	  for (var word in mobileKeyWords) {
 		if (navigator.userAgent.match(mobileKeyWords[word]) != null) {
-			alert("모바일로 접속시 화면이 깨지는 현상이 일어날 수 있습니다.");
+			alert("모바일로 접속시 리다이렉트가 불가할 수 있으며,\n화면 최적화가 되지 않아 화면이 깨지는 현상이 발생합니다.");
 		   break;
 	  }
 	}
